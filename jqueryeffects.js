29bb5951f082c28c3ef4eeefc00e83df303ec455
Denzel Pressey <pressey90@gmail.com>
2013-08-10 16:39:05 -0400

    first commit

---
+++ jqueryeffects.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$("#button").on('click', function(){
+		$("#blue").slideToggle("fast");
+		$("#yellow").slideToggle("fast");
+		$("#red").slideToggle("fast");
+		$("#green").slideToggle("fast");
+		$("#black").slideToggle("fast");
+		$("#pink").slideToggle("fast");
+	});	
+});
+
